0d39b0af5248c9d351e970dc016eeed22672d2c6
EZZANEGA BOUCHRA <ezzanegabouchra@gmail.com>
2026-04-23 13:51:37 +0100

    Auto-generated commit

---
+++ chunk-UJKXABKD.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-2GJ246N4.js";import"./chunk-MZNIYXBP.js";import"./chunk-DGOT5X44.js";export{a as ShopModule};
--- chunk-X47H4ZEX.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-ALNIAXLY.js";import"./chunk-MZNIYXBP.js";import"./chunk-DGOT5X44.js";export{a as ShopModule};
