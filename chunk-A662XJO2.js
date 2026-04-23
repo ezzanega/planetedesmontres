5b5aaeebb0911bb55aec66fe22614bc5b133e593
EZZANEGA BOUCHRA <ezzanegabouchra@gmail.com>
2026-04-23 13:35:41 +0100

    Auto-generated commit

--- chunk-A662XJO2.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-Z37ZELFN.js";import"./chunk-2J22NNA3.js";import"./chunk-DG5AQCSS.js";export{a as ShopModule};
---
+++ chunk-TG7QIHZR.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-CSRIKNDL.js";import"./chunk-JI5ZLJVF.js";import"./chunk-NWMDCPU2.js";export{a as ShopModule};
